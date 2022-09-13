260b6c7bd869f115649170815a6c2abeb8eb0b28
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2022-09-13 17:39:50 +0200

    created expressJS app and modify readme file

--- noteblock/src/App.jsx
+++ noteblock/src/App.jsx
@@ -1,11 +1,12 @@
-import TextArea from "./components/textArea/TextArea.jsx";
+// import TextArea from "./components/textArea/TextArea.jsx";
 import "./App.css";
+// import TodoApp from "./components/textArea/TextArea.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="title">NoteBlock</h2>
-      <TextArea />
+      {/* <h2 className="title">NoteBlock</h2>
+      <TodoApp /> */}
     </div>
   );
 }
---
+++ server/backend/README.md
@@ -0,0 +1,21 @@
+# NoteBlock
+
+# Notes application with ReactJS, mongoDB, NodeJS and ExpressJS.
+
+## 1st step, install dependencies inside the server/backend directory and noteblock
+
+```
+npm install
+```
+
+## initialize noteblock application
+
+```
+npm start
+```
+
+## Initialize backend
+
+```
+npm start
+```
