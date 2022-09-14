2035ef79491e7f9d423df9d735cb3b961f37f0e6
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2022-09-14 13:53:21 +0200

    bootstrap form

--- noteblock/src/components/form/form.jsx
+++ noteblock/src/components/form/form.jsx
@@ -4,6 +4,34 @@ export default function Form() {
   return (
     <div className="card">
       <div className="card-header">Add note</div>
+      <div className="card-body">
+        <form action="">
+          <div className="form-group">
+            <input
+              name="title"
+              type="text"
+              placeholder="Title"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              name="content"
+              placeholder="Task content"
+              className="form-control"
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-outline-success btn-sm btn-block"
+          >
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- noteblock/src/pages/index.jsx
+++ noteblock/src/pages/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Form from "../components/form/Form";
 import ListGroup from "../components/list/ListGroup";
 
 export default function Index() {
   return (
-    <div className="content-app t-50">
+    <div className="content-app ">
       <div className="row ">
-        <div className="col-sm-12 col-md-4">form</div>
+        <div className="col-sm-12 col-md-4">
+          <Form />
+        </div>
         <div className="col-sm-12 col-md-8">
           <ListGroup />
         </div>
